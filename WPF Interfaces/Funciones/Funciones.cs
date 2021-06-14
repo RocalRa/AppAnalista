fb351c709464886f288ac92710a073d890f22ef9
Rogelio Rocha <rogelio.2.rocha@aptiv.com>
2021-06-14 13:42:57 -0600

    Integracion de Funcion de Login, e interfaz basica de la misma

---
+++ WPF Interfaces/Funciones/Funciones.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WPF_Interfaces
+{
+	public class Funciones
+	{
+		public void Login(string txtLogonId, string txtPswrd)
+		{
+			string UserLoged = txtLogonId;
+			Clases_y_Funciones_VB.Funciones_VB vb = new Clases_y_Funciones_VB.Funciones_VB();
+			if (vb.AuthenticateUser("APTIV", txtLogonId, txtPswrd))
+			{
+				// Cambio a Pagina Principal
+				MessageBox.Show("Sesion Iniciada");
+				//MainWindow mw  = new MainWindow();
+				//mw.Show();
+				//this.Close();
+			}
+			else
+			{
+				txtPswrd = "";
+				MessageBox.Show("Contrasena Incorrecta");
+			}
+		}
+
+
+
+
+	}
+}
---
+++ WPF Interfaces/Vistas/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Interfaces.Vistas
+{
+	/// <summary>
+	/// Interaction logic for LoginPage.xaml
+	/// </summary>
+	public partial class LoginPage : Page
+	{
+		public LoginPage()
+		{
+			InitializeComponent();
+		}
+
+		Funciones fn = new Funciones();
+
+	
+		private void btnIngresar_Click(object sender, RoutedEventArgs e)
+		{
+			fn.Login(txtBoxUser.Text,pwdBox.Password);
+		}
+	}
+}
